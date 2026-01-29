73699156445c017520b12c5eb46b2add9d7bb5f7
Jorge Ubirajara Pedreira Junior <jorgesars@gmail.com>
2026-01-29 13:20:10 -0300

    Inject dependency chunk so pkgdown includes htmlwidget JS/CSS
    Pre-computed .Rmd files contain embedded htmlwidget HTML but pkgdown
    does not include the JavaScript/CSS libraries because no widget object
    is created during its render pass. Fix by injecting a hidden executable
    chunk (include=FALSE) that creates a dummy mapview widget, forcing
    rmarkdown to include leaflet/mapview/leafsync JS/CSS in the page head.

--- vignettes/articles/_build.R
+++ vignettes/articles/_build.R
@@ -16,4 +16,34 @@ for (f in orig_files) {
   output <- sub("\\.orig$", "", f)
   message("Knitting ", f, " -> ", output)
   knitr::knit(f, output)
+
+  # Post-process: inject an executable chunk that forces pkgdown to include
+
+  # htmlwidget JS/CSS dependencies when it renders the pre-computed .Rmd.
+  # Without this, the embedded widget HTML has no JavaScript to render it.
+  lines <- readLines(output)
+  uses_leafsync <- any(grepl("leafsync", readLines(f)))
+
+  dep_chunk <- c(
+    "",
+    "```{r include=FALSE}",
+    "# Hidden chunk to ensure pkgdown includes htmlwidget dependencies",
+    "library(mapview)"
+  )
+  if (uses_leafsync) {
+    dep_chunk <- c(dep_chunk, "library(leafsync)")
+  }
+  dep_chunk <- c(
+    dep_chunk,
+    "mapview::mapview()",
+    "```",
+    ""
+  )
+
+  # Insert after YAML front matter (second '---' line)
+  yaml_markers <- which(lines == "---")
+  insert_pos <- yaml_markers[2]
+  lines <- c(lines[1:insert_pos], dep_chunk, lines[(insert_pos + 1):length(lines)])
+  writeLines(lines, output)
+  message("  Injected dependency chunk into ", output)
 }
