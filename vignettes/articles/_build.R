522226bb15d0d0babb9743c6a542455d12c2f374
Jorge Ubirajara Pedreira Junior <jorgesars@gmail.com>
2026-01-29 12:47:05 -0300

    Fix pre-computed articles and bump version to 0.2.0
    - Set knitr output to HTML mode so htmlwidgets (mapview, leafsync)
      embed directly instead of failing with path.expand error
    - Add devtools::load_all() to _build.R so package is loaded before knit
    - Add error = FALSE to all .Rmd.orig setup chunks to halt on errors
    - Re-knit all 5 articles: 0 errors, 21 htmlwidgets properly embedded
    - Bump version to 0.2.0 so pkgdown deploys to the root site (mode: auto
      treats .9000 versions as dev builds under /dev/)

--- vignettes/articles/_build.R
+++ vignettes/articles/_build.R
@@ -1,6 +1,15 @@
 # Re-knit all pre-computed articles
-# Run from the vignettes/articles/ directory:
-#   source("_build.R")
+# Run from the repo root:
+#   source("vignettes/articles/_build.R")
+
+devtools::load_all()
+
+owd <- setwd("vignettes/articles")
+on.exit(setwd(owd))
+
+# Tell knitr the output target is HTML so htmlwidgets (mapview, leafsync)
+# embed their HTML directly instead of attempting a screenshot.
+knitr::opts_knit$set(rmarkdown.pandoc.to = "html")
 
 orig_files <- list.files(pattern = "\\.Rmd\\.orig$")
 for (f in orig_files) {
